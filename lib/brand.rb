64e65d01b020f2cd7b07d923e1d003fb980fa544
Dan Jacob <dansamueljacob@gmail.com>
2017-05-18 18:51:32 -0700

    add capitalize method for brands

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,16 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   validates(:name, {:presence => true, :uniqueness => true, :length => {:maximum => 100}})
+  before_save(:upcase_name)
+
+  scope(:not_done, -> do
+    where({:done => false})
+  end)
+
+
+  private
+
+  define_method(:upcase_name) do
+    self.name=(name().capitalize())
+  end
 end

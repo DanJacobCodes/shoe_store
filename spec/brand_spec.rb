29b729c25ae0ab4cddec5784f1a987e357998b4a
Dan Jacob <dansamueljacob@gmail.com>
2017-05-18 19:10:51 -0700

    pass all specs for Store class for same callback as brand as well validations

---
+++ spec/brand_spec.rb
@@ -0,0 +1,27 @@
+require("spec_helper")
+
+describe(Brand)do
+  it("validates presece of brand name") do
+    brand = Brand.new({:name => ""})
+    expect(brand.save()).to(eq(false))
+  end
+end
+describe(Brand) do
+    it("converts the first letter to uppercase") do
+      brand = Brand.create({:name => "nike"})
+      expect(brand.name()).to(eq("Nike"))
+    end
+  end
+  describe(Brand) do
+it("ensures the length of name is at most 100 characters") do
+  brand = Brand.new({:name => "a".*(101)})
+  expect(brand.save()).to(eq(false))
+  end
+end
+describe(Brand) do
+it("ensures that no two brands are alike") do
+  brand = Brand.new({:name => "Nike"})
+  brand2 = Brand.new({:name => "Nike"})
+  expect(brand.save()).to(eq(false))
+    end
+  end
---
+++ spec/store_spec.rb
@@ -0,0 +1,26 @@
+require("spec_helper")
+describe(Store)do
+  it("validates presece of store name") do
+    store = Store.new({:name => ""})
+    expect(store.save()).to(eq(false))
+  end
+end
+describe(Store) do
+    it("converts the first letter to uppercase") do
+      store = Store.create({:name => "nike"})
+      expect(store.name()).to(eq("Nike"))
+    end
+  end
+  describe(Store) do
+it("ensures the length of name is at most 100 characters") do
+  store = Store.new({:name => "a".*(101)})
+  expect(store.save()).to(eq(false))
+  end
+end
+describe(Store) do
+it("ensures that no two stores are alike") do
+  store = Store.new({:name => "Nike"})
+  store2 = Store.new({:name => "Nike"})
+  expect(store.save()).to(eq(false))
+    end
+  end

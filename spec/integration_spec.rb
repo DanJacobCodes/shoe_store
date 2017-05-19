7533b6b24a3ad2574ce6e4cc41e93a647995d66b
Dan Jacob <dansamueljacob@gmail.com>
2017-05-18 19:09:32 -0700

    pass specs for character limit and uniquness on Brand class

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -12,3 +12,16 @@ describe(Brand) do
       expect(brand.name()).to(eq("Nike"))
     end
   end
+  describe(Brand) do
+it("ensures the length of name is at most 100 characters") do
+  brand = Brand.new({:name => "a".*(101)})
+  expect(brand.save()).to(eq(false))
+  end
+end
+describe(Brand) do
+it("ensures that no two brands are alike") do
+  brand = Brand.new({:name => "Nike"})
+  brand2 = Brand.new({:name => "Nike"})
+  expect(brand.save()).to(eq(false))
+    end
+  end
